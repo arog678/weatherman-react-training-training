ac2f68f076a6a29ea4d04dd36335448db735ae7a
arog678 <39784723+arog678@users.noreply.github.com>
2024-08-19 00:06:02 +1200

    remove basename

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import NavigateSetter from './modules/navigation/components/NavigateSetter'
 
 const App = (): JSX.Element => (
 	<Provider store={store}>
-		<BrowserRouter basename='/weatherman-react-training-training/'>
+		<BrowserRouter>
 			<NavigateSetter />
 			<LazySwitch>
 				<Route path={Paths.HOME} element={<Routes.Search />} />
